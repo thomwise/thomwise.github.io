8cf1f80a0ceb04fab7012833db32e20542c17b44
Thomas Wise <Thomas Wise>
2017-05-16 15:28:41 -0700

    working on adding slide in feature using CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
 (function() {
+// SLIDE EFFECT
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+  console.log(sliderImages);
+function checkSlide(e) {
+sliderImages.forEach(sliderImage => {
+  // half way through the image
+  const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+  // bottom of the image
+  const imageBottom = sliderImage.offsetTop +sliderImage.height;
+  const isHalfShown = slideInAt > sliderImage.offsetTop;
+  const isNotScrolledPast = window.scrollY < imageBottom;
+  console.log(slideInAt);
+
+  if (isHalfShown && isNotScrolledPast) {
+    sliderImage.classList.add('active');
+  } else {
+    sliderImage.classList.remove('active');
+  }
+});
+}
+
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+
+// FORM SECTION
   var myForm = document.getElementById('emailForm'),
       submitButton = document.querySelector('form button'),
 
@@ -23,6 +64,22 @@ nameInput.addEventListener('blur', function(e) {
         nameInput.classList.remove('danger');
         submitButton.disabled = false;
       }
+});
+
+emailInput.addEventListener('blur', function(e) {
+  var myEmail = emailInput.value,
+      validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(myEmail);
+
+  if (!validEmail) {
+    emailInput.classList.add('danger');
+  } else {
+    emailInput.classList.remove('danger');
+  }
+});
+
+myForm.addEventListener('submit', function(e) {
+  document.getElementById('comment').value.trim();
+  myForm.setAttribute('action', 'https://formspree.io/thomas.ja.wise@gmail.com');
 });
 
 }());
